5db0d5962c9ee0eae92a1f1879da152a98b1647e
Carlos Salazar <crslarez@gmail.com>
2021-10-11 13:05:00 -0400

    Comenzando la lógica

---
+++ scripts/main.js
@@ -0,0 +1,50 @@
+entUsu = Number(document.getElementById("entrada").value)
+priMon = document.getElementById("monedaEnt").value
+segMon = document.getElementById("monedaSal").value
+
+
+function validar(){
+    if 
+    
+}
+//console.log(entUsu,priMon,segMon);
+
+let dolar = [
+    37,
+    47,
+    3,
+    20
+  ]
+
+
+  let euro = [
+    37,
+    47,
+    3,
+    20
+  ]
+
+  
+let colpes = [
+    37,
+    47,
+    3,
+    20
+  ]
+
+  
+let libes = [
+    37,
+    47,
+    3,
+    20
+  ]
+
+  
+let mexpes = [
+    37,
+    47,
+    3,
+    20
+  ]
+
